ccf751b794dace7f907643e9de8ac9668391d56a
balami <luxmeb@gmail.com>
2021-02-23 13:05:21 +0100

    check to see if already replied

--- reddit_bot.py
+++ reddit_bot.py
@@ -10,11 +10,14 @@ def get_random_quotes():
 
 
 subreddit = reddit.subreddit("all")
+already_replied = []
+
 for submission in subreddit.search(query="poem", time_filter="day", sort="top", limit=10):
     title = submission.title.lower()
-    if "poem" in title:
+    if "poem" in title and submission.id not in already_replied:
         print(title)
         fn_response = get_random_quotes()
         if fn_response.status_code == 200:
             submission.reply((fn_response.json())[0]['content'])
+            already_replied.append(submission.id)
         break
