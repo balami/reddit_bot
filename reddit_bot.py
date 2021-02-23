49d1e50d097a42be1e109b1ae7fb37e020aabe3b
balami <luxmeb@gmail.com>
2021-02-23 17:05:06 +0100

    refactor using functions

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,37 +1,43 @@
 import requests
 import praw
 
-'''
-Pass app params from PRAW INI file to instantiate. Replace the site_name value by your own site name.
-'''
-reddit = praw.Reddit(site_name="default_bot", config_interpolation="basic")
 
-'''
-Function to get random poems from the POEMIST API. The call response is a json with 5 items.
-'''
+def praw_config():
+    """
+    Pass app params from PRAW INI file to instantiate. Replace the site_name value by your own site name.
+    """
+    r = praw.Reddit(site_name="default_bot", config_interpolation="basic")
+    return r
 
 
 def get_random_quotes():
+    """
+    Get random poems from the POEMIST API.
+    """
     response = requests.get("https://www.poemist.com/api/v1/randompoems")
     return response
 
 
-'''Pass the subreddit name as the param to the method. 
-To check if we already replied, instantiate an empty list to store submission IDs which are unique for all submissions.
- '''
+def post_reply(reddit):
+    """Pass the search query and if a submission contains the word and is not already replied, post a poem as a reply.
+    """
+    subreddit = reddit.subreddit("all")
+    already_replied = []
 
-subreddit = reddit.subreddit("all")
-already_replied = []
+    # The params time_filter, sort and limit are optional.
+    for submission in subreddit.search(query="poem", time_filter="week", sort="top", limit=50):
+        title = submission.title.lower()
+        if "poem" in title and submission.id not in already_replied:
+            fn_response = get_random_quotes()
+            # only continue if the API call to POEMIST was successful.
+            if fn_response.status_code == 200:
+                # reply to the submission
+                submission.reply((fn_response.json())[0]['content'])
+                # store the submission ID so that we don't reply again
+                already_replied.append(submission.id)
+                # to the same submission.
 
-'''Pass the search string and other optional parameters like sorting by top submissions, limiting the result to only n 
-number, showing search results only from within 24 hour and so on'''
 
-for submission in subreddit.search(query="poem", time_filter="day", sort="top", limit=10):
-    title = submission.title.lower()
-    if "poem" in title and submission.id not in already_replied:
-        fn_response = get_random_quotes()
-        if fn_response.status_code == 200:  # only continue if the API call to POEMIST was successful.
-            submission.reply((fn_response.json())[0]['content'])  # reply to the submission
-            already_replied.append(submission.id)  # store the submission ID so that we don't reply again
-            # to the same submission.
-        break
+if __name__ == "__main__":
+    reddit_config = praw_config()
+    post_reply(reddit_config)
